d5b7cca8a43091db6e59925ca8626d409ca061ce
abdi eid <cabdiwalibashir232@gmail.com>
2022-11-09 20:16:57 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in); //system in waa keyboardka systemoutna waa shaashada.
+//        System.out.println(input.nextInt());
+//        System.out.println("enter your age");
+//        System.out.println("your age is:"+input.next());
+//        int rolling=33;
+//        float floating=33.1f;
+//        char   charing='r';
+//        double largedecimalnumbers=2233.2;
+//        long longinteger=33333388883L;
+//        boolean check=true;
+//    int sox=233_000_00;
+//        System.out.println(sox);
+//     int num1=input.nextInt();
+//     int num2=input.nextInt();
+//     int sum=num1+num2;
+//        System.out.println("sum:"+sum);
+
+//        ,markaad garanaysid inta jeer ee loopku luupaynayo isticmaal for , hadaadan garnayn isticmaal while.
+
+    }
+}
